ad12b4cc67304714bc02bdfcc3ce23e70d5561da
JOUHAULT Quentin <quentin.jouhault@viveris.fr>
2024-03-08 17:06:51 +0100

    start modal

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,8 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { Checklist } from '../shared/interfaces/ichecklist';
+import { ModalComponent } from '../shared/ui/modal/modal.component';
 
 @Component({
   standalone: true,
   selector: 'app-home',
-  template: ` <p>Coucou</p> `,
+  imports: [ModalComponent],
+  template: ` 
+     <header>
+      <h1>Quicklists</h1>
+      <button (click)="checklistBeingEdited.set({})">Add Checklist</button>
+    </header>
+
+    <app-modal [isOpen]="!!checklistBeingEdited()">
+      <ng-template> You can't see me... yet </ng-template>
+    </app-modal>
+  `,
 })
-export default class HomeComponent { }
+export default class HomeComponent {
+  checklistBeingEdited = signal<Partial<Checklist> | null>(null);
+}
